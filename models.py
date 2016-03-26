5841091477b5b546b9524a1d77b2b3b07f77d1be
David Diz Oya <ddizoya@danielcastelao.org>
2016-03-26 19:08:55 +0100

    Hecha la parte de las dependencias donde se comprueban el % de asientos cogidos.

--- models.py
+++ models.py
@@ -24,3 +24,13 @@ class Session(models.Model):
         domain=[('instructor', '=', True),   ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Curso", required=True)
+
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
